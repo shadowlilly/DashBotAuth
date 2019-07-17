b3cbac5222d24df24baa242e3786e2a612162f0c
Lilly <shadowlilly013@gmail.com>
2019-07-17 00:47:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ socket = new webServer({
 
 socket.on('request', function(request) {
 
+    console.log("A");
+
     socket.on('connect', function(connection) {
+
+     console.log("B");
+
       var connection = request.accept('DBCP-KEY_' + process.env.socketkey, request.origin);
       /*connection.sendUTF("TOKEN IS " + process.env.localtoken, function(err, res) {
         if(err) throw err;
