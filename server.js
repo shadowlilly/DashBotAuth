2c2df84be7c8e5dab98c7cb0e2f99ef76a691840
Lilly <shadowlilly013@gmail.com>
2019-07-17 09:11:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ socket.on('request', function(request) {
 
     connection = request.accept("", request.origin);
 
-    connection.sendUTF("TOKEN IS " + process.env.localtoken).catch(function(err){
-      throw err;
-    });
+    connection.sendUTF("TOKEN IS " + process.env.localtoken);
     console.log((new Date()) + ' Connection accepted.');
 
     connection.on('message', function(message) {
