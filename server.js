1eb0feb671075b933f40a61b765fdeac0e940e50
Lilly <shadowlilly013@gmail.com>
2019-07-17 09:52:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ socket = new webServer({
 
 socket.on('request', function(request) {
 
-    connection = request.accept("", "");
+    connection = request.accept("DBCP", "DBCP");
 
     connection.sendUTF("TOKEN IS '" + process.env.localtoken + "'");
     console.log((new Date()) + ' Connection accepted.');
