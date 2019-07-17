35800dcae8d87ac28a498ea46b99c266863e1af8
Lilly <shadowlilly013@gmail.com>
2019-07-16 18:03:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,24 @@ function originIsAllowed(origin) {
   return origin == "https://dashbot0013.herokuapp.com"
 }
 
-socket.on('connect', function(connection) {
-  var connection = request.accept('DBCP', request.origin);
-  console.log((new Date()) + ' Connection accepted.');
-  connection.on('message', function(message) {
-    console.log('Received Message: ' + message.utf8Data);
-      connection.sendUTF(message.utf8Data);
+socket.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    socket.on('connect', function(connection) {
+      var connection = request.accept('DBCP', request.origin);
+      console.log((new Date()) + ' Connection accepted.');
+      connection.on('message', function(message) {
+        console.log('Received Message: ' + message.utf8Data);
+          connection.sendUTF(message.utf8Data);
+        });
+        connection.on('close', function(reasonCode, description) {
+          console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        });
     });
-    connection.on('close', function(reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+
 });
