3bb60c6154b9cd591d5c5b863556a33a6ae96b3d
Lilly <shadowlilly013@gmail.com>
2019-07-17 11:11:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 var webServer = require('websocket').server;
+const { Client } = require('pg');
+bool connected = false;
+const pgclient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+pgclient.connect();
 
 var server = http.createServer(function (request, response) {
 
@@ -15,9 +22,32 @@ socket = new webServer({
     autoAcceptConnections: false
 });
 
+function checkLogin() {
+
+  if(!connected) {
+    pgclient.query("UPDATE keys SET lockdown = true").then(function() {
+      process.exit(0);
+    })
+  }
+
+}
+
+window.setTimeout(checkLogin, 60000);
+
 socket.on('request', async function(request) {
 
+    var value = await pgclient.query("SELECT lockdown FROM keys LIMIT 1").then(function(res)) {
+      return res.rows[0].lockdown;
+    }
+
+    if(value) {
+      request.reject();
+      process.exit(0);
+    }
+
     connection = request.accept("dbcp_key-" + process.env.socketkey, request.origin);
+
+    connected = true;
 
     console.log((new Date()) + ' Connection accepted.');
 
@@ -31,6 +61,9 @@ socket.on('request', async function(request) {
     });
 
     connection.on('close', function(reasonCode, description) {
+      pgclient.query("UPDATE keys SET lockdown = true").then(function() {
+        process.exit(0);
+      })
       console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 
